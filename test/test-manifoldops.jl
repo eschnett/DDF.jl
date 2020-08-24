b383ffc32a97af151dfab65e6eb52b136804df02
Erik Schnetter <schnetter@gmail.com>
2020-08-24 19:51:09 -0400

    More stringent operator tests

--- test/test-manifoldops.jl
+++ test/test-manifoldops.jl
@@ -4,19 +4,17 @@ using StaticArrays
 using Test
 
 @testset "Manifold ops D=$D P=$P R=$R" for D in 0:Dmax, P in (Pr, Dl), R in 0:D
-    T = Rational{Int128}
+    S = Rational{Int128}
 
-    mfds = [empty_manifold(Val(D), T), simplex_manifold(Val(D), T),
-            hypercube_manifold(Val(D), T)]
+    mfds = [empty_manifold(Val(D), S), simplex_manifold(Val(D), S),
+            hypercube_manifold(Val(D), S)]
 
     for mfd in mfds
-        funs = Fun{D,P,R,T,T}[]
-        push!(funs, zero(Fun{D,P,R,T,T}, mfd))
-        e = id(Fun{D,P,0,T,SVector{D,T}}, mfd)
+        funs = Fun{D,P,R,S}[]
+        push!(funs, zero(Fun{D,P,R,S,S}, mfd))
+        e = id(Fun{D,P,0,S,SVector{D,S}}, mfd)
         if R == 0
-            for d in 1:D
-                push!(funs, map(x -> x[d],  e))
-            end
+            push!(funs, e)
         end
 
         R1 = P == Pr ? R - 1 : R + 1
@@ -24,7 +22,8 @@ using Test
             b = boundary(Val(P), Val(R), mfd)
             for f in funs
                 bf = b * f
-                bf::Fun{D,P,R1,T,T}
+                T = eltype(f)
+                bf::Fun{D,P,R1,S,T}
             end
         end
 
@@ -33,7 +32,8 @@ using Test
             d = deriv(Val(P), Val(R), mfd)
             for f in funs
                 df = d * f
-                df::Fun{D,P,R1,T,T}
+                T = eltype(f)
+                df::Fun{D,P,R1,S,T}
             end
         end
     end
