d895510f1c979b83eaadb02ca95a70e2b5ec99c4
Erik Schnetter <schnetter@gmail.com>
2020-08-24 16:28:07 -0400

    Remove unused algorithms (moved to ManifoldConstructors)

--- src/Algorithms.jl
+++ src/Algorithms.jl
@@ -1,8 +1,6 @@
 module Algorithms
 
-using ComputedFieldTypes
 using DifferentialForms
-using SparseArrays
 using StaticArrays
 
 # function circumcentre1(xs::SVector{R, <:Chain{V, 1, T}}) where {R, V, T}
@@ -44,36 +42,6 @@ function circumcentre(xs::SVector{N,<:Form{D,1,T}}) where {N,D,T}
     c = A \ b
     cc = sum(c[i] * xs[i] for i in 1:N)
     return cc::Form{D,1,T}
-end
-
-export regular_simplex
-# Generate the coordinate positions for a regular simplex with edge
-# length 1.
-#
-# The algorithm proceeds recursively. A 0D simplex is a point. A
-# D-simplex is a D-1-simplex that is shifted down along the new axis,
-# plus a new point on the new axis.
-function regular_simplex1(D::Int, ::Type{T}) where {T}
-    @assert D >= 0
-    N = D + 1
-    # D == 0 && return SVector{N}(Chain{V,1,T}())
-    @assert D > 0
-    D == 1 && return SVector(Form{D,1}((T(-1) / 2,)), Form{D,1}((T(1) / 2,)))
-    s0 = regular_simplex1(D - 1, T)
-    # Choose height so that edge length is 1
-    z = sqrt(1 - abs2(s0[1]))
-    z0 = -z / (D + 1)
-    s = SVector{N}(map(x -> x ⊕ Form{1,1,T}((z0,)), s0)...,
-                   zero(Form{D - 1,1,T}) ⊕ Form{1,1,T}((z + z0,)))
-    return s::SVector{N,fulltype(Form{D,1,T})}
-end
-@generated function regular_simplex(::Type{<:Form{D,R,T}}) where {D,R,T}
-    D::Int
-    @assert D >= 0
-    R::Int
-    @assert 0 <= R <= D
-    @assert R == D              # we could relax this
-    return regular_simplex1(D, T)
 end
 
 end
