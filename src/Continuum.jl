e9dd77c33d86fa195c3f81447171bf0f84944ac9
Erik Schnetter <schnetter@gmail.com>
2020-08-28 18:39:34 -0400

    Implement projecting functions onto manifold

--- src/Continuum.jl
+++ src/Continuum.jl
@@ -4,31 +4,12 @@ using Bernstein
 using ComputedFieldTypes
 using DifferentialForms
 using NearestNeighbors
+using SimplexQuad
 using StaticArrays
 
 using ..Funs
 using ..Manifolds
 using ..SparseOps
-
-################################################################################
-
-export sample
-"""
-Sample a function on a manifold
-"""
-function sample(::Type{<:Fun{D,P,R,S,T}}, f,
-                mfd::Manifold{D,S}) where {D,P,R,S,T}
-    @assert P == Pr             # TODO
-    @assert R == 0              # TODO
-    values = Array{T}(undef, nsimplices(mfd, R))
-    for i in 1:nsimplices(mfd, R)
-        x = SVector{D,S}(@view mfd.coords[i, :])
-        y = f(x)
-        y::Form{D,R,T}
-        values[i] = y[]::T
-    end
-    return Fun{D,P,R,S,T}(mfd, values)
-end
 
 ################################################################################
 
@@ -54,10 +35,9 @@ function evaluate(f::Fun{D,P,R,S,T}, x::SVector{D,S}) where {D,P,R,S,T}
         sj = sparse_column_rows(mfd.simplices[D], j)
         @assert length(sj) == N
         sj = SVector{D + 1,Int}(sj[n] for n in 1:N)
-
-        # Coordinates of sjmplex vertices
-        # xs = map(k -> SVector{D,S}(@view coords[k, :]) for k in sj)
-        # xs::SVector{N,SVector{D,S}}
+        # Coordinates of simplex vertices
+        # This is only correct for D == 0, P == Pr
+        @assert D == 0 && P == Pr
         xs = SVector{N,SVector{D,S}}(SVector{D,S}(@view mfd.coords[k, :])
                                      for k in sj)
         # setup = cartesian2barycentric_setup(xs)
@@ -77,11 +57,108 @@ function evaluate(f::Fun{D,P,R,S,T}, x::SVector{D,S}) where {D,P,R,S,T}
     @error "Coordinate $x not found in manifold $(mfd.name)"
 end
 
+################################################################################
+
+export sample
+"""
+Sample a function on a manifold
+"""
+function sample(::Type{<:Fun{D,P,R,S,T}}, f,
+                mfd::Manifold{D,S}) where {D,P,R,S,T}
+    @assert P == Pr             # TODO
+    @assert R == 0              # TODO
+    values = Array{T}(undef, nsimplices(mfd, R))
+    for i in 1:nsimplices(mfd, R)
+        x = SVector{D,S}(@view mfd.coords[i, :])
+        y = f(x)
+        y::Form{D,R,T}
+        values[i] = y[]::T
+    end
+    return Fun{D,P,R,S,T}(mfd, values)
+end
+
+################################################################################
+
+export project
+"""
+Project a function onto a manifold
+"""
+function project(::Type{<:Fun{D,P,R,S,T}}, f,
+                 mfd::Manifold{D,S}) where {D,P,R,S,T}
+    @assert P == Pr             # TODO
+    @assert R == 0              # TODO
+
+    if D == 0
+        values = T[f(SVector{D,S}())[]]
+        return Fun{D,P,R,S,T}(mfd, values)
+    end
+
+    order = 4                   # Choice
+
+    N = D + 1
+
+    f(zero(SVector{D,S}))::Form{D,R,T}
+
+    values = Array{T}(undef, nsimplices(mfd, R))
+    # Loop over all R-simplices
+    for i in 1:nsimplices(mfd, R)
+        value = zero(T)
+        # Loop over the support of the R-simplex's basis functions,
+        # i.e. all neighbouring top simplices
+        for j in sparse_column_rows(mfd.lookup[(D, R)], i)
+            sj = sparse_column_rows(mfd.simplices[D], j)
+            @assert length(sj) == N
+            sj = SVector{N,Int}(sj[n] for n in 1:N)
+            # Coordinates of simplex vertices
+            xs = SVector{N,SVector{D,S}}(SVector{D,S}(@view mfd.coords[k, :])
+                                         for k in sj)
+            xs::SVector{N,SVector{D,S}}
+            setup = cartesian2barycentric_setup(xs)
+
+            # Find integration method
+            XS = S[xs[n][d] for n in 1:N, d in 1:D]
+            XS::Array{S,2}
+            X, W = simplexquad(order, [xs[n][d] for n in 1:N, d in 1:D])
+
+            # `findfirst` here works only for D == 0
+            @assert D == 0
+            n = findfirst(==(i), sj)
+            @assert n !== nothing
+            # `[]` here works only for D == 0
+            @assert D == 0
+            kernel(x::SVector{D,T}) = basis_x(setup, n, x) * f(x)[]
+
+            bf = integrate_x(kernel, Val(D), X, W)::T
+            value += bf
+        end
+        values[i] = value
+    end
+    @error ""
+    return Fun{D,P,R,S,T}(mfd, values)
+end
+
+################################################################################
+
 # TODO: Remove this, use Bernstein polynomials instead
 @fastmath function basis_λ(n::Int, λ::SVector{N,S}) where {N,S}
     N::Int
     @assert 1 <= n <= N
     return λ[n]
+end
+
+@fastmath function integrate_x(::Val{D}, f, X, W) where {D}
+    D::Int
+    @assert D > 0
+    @assert size(X, 2) == D
+    @assert size(X, 1) == size(W, 1)
+
+    @inbounds begin
+        s = zero(W[1]) * f(SVector{D}(@view X[1, :]))
+        for n in 1:length(W)
+            s += W[n] * f(SVector{D}(@view X[n, :]))
+        end
+        return s
+    end
 end
 
 end
--- test/test-continuum.jl
+++ test/test-continuum.jl
@@ -4,6 +4,34 @@ using ComputedFieldTypes
 using DifferentialForms
 using StaticArrays
 
+@testset "Evaluate a function D=$D P=$P R=$R" for D in 0:Dmax,
+P in (Pr, Dl),
+R in 0:D
+
+    # TODO: all D, all P
+    (D == 0 && P == Pr) || continue
+
+    S = Float64
+    mfd = simplex_manifold(Val(D), S)
+
+    T = S
+
+    f0 = zero(Fun{D,P,R,S,T}, mfd)
+    nvalues = nsimplices(mfd, P == Pr ? R : D - R)
+    f1 = Fun{D,P,R,S,T}(mfd, [T(i) for i in 1:nvalues])
+    f2 = Fun{D,P,R,S,T}(mfd, rand(T, nvalues))
+    a = rand(T)
+
+    for i in 1:nsimplices(mfd, P == Pr ? R : D - R)
+        x = SVector{D,S}(@view mfd.coords[i, :])
+        @test evaluate(f0, x) == zero(Form{D,R,T})
+        @test evaluate(f1 + f2, x) ≈ evaluate(f1, x) + evaluate(f2, x)
+        @test evaluate(a * f1, x) ≈ a * evaluate(f1, x)
+    end
+
+    @warn "evaluate at random points"
+end
+
 @testset "Sample a function D=$D P=$P R=$R" for D in 0:Dmax,
 P in (Pr, Dl),
 R in 0:D
@@ -31,13 +59,49 @@ R in 0:D
     for i in 1:nsimplices(mfd, R)
         x = SVector{D,S}(@view mfd.coords[i, :])
         fx = f(x)::Form{D,R,T}
-        f̃x = evaluate(f̃, x)
-        f̃x::Form{D,R,T}
-        Ex = norm(f̃x - fx)
-        @test 1 + Ex ≈ 1
+        if D == 0               # TODO: evaluate requires D == 0
+            f̃x = evaluate(f̃, x)
+            f̃x::Form{D,R,T}
+            Ex = norm(f̃x - fx)
+            @test 1 + Ex ≈ 1
+        end
     end
+
+    @warn "evaluate at random points"
 end
 
-@warn "CONTINUE HERE"
+@testset "Project a function D=$D P=$P R=$R" for D in 0:Dmax,
+P in (Pr, Dl),
+R in 0:D
 
-@testset "evaluate" begin end
+    # TODO: all D, all R, all P
+    (D == 0 && R == 0 && P == Pr) || continue
+
+    S = Float64
+    mfd = simplex_manifold(Val(D), S)
+
+    # f(x) = Form{D,R,S}((prod(sin(2π * x[d]) for d in 1:D),))
+    b = rand(Form{D,0,S})
+    if D == 0
+        f = x -> b
+    else
+        m = rand(Form{D,1,S})
+        f = x -> m ⋅ Form{D,1,S}(x) + b
+    end
+    T = S
+    f(zero(SVector{D,S}))::Form{D,R,T}
+    f̃ = project(Fun{D,P,R,S,T}, f, mfd)
+
+    for i in 1:nsimplices(mfd, R)
+        x = SVector{D,S}(@view mfd.coords[i, :])
+        fx = f(x)::Form{D,R,T}
+        if D == 0               # TODO: evaluate requires D == 0
+            f̃x = evaluate(f̃, x)
+            f̃x::Form{D,R,T}
+            Ex = norm(f̃x - fx)
+            @test 1 + Ex ≈ 1
+        end
+    end
+
+    @warn "evaluate at random points"
+end
