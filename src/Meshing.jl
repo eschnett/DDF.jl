e5b0d36fe73b14a2eb9b8db61c2ba296523aad7d
Erik Schnetter <schnetter@gmail.com>
2020-10-29 14:33:56 -0400

    Use MiniQhull instead of Delauney

--- src/Meshing.jl
+++ src/Meshing.jl
@@ -1,7 +1,8 @@
 module Meshing
 
-using Delaunay
+# using Delaunay
 using DifferentialForms
+using MiniQhull
 using SparseArrays
 using StaticArrays
 
@@ -24,26 +25,52 @@ function delaunay_mesh(coords::Vector{SVector{C,S}}) where {C,S}
         return simplices
     end
 
-    # Triangulate
-    mesh = delaunay(S[coords[i][c] for i in 1:nvertices, c in 1:C])
-    # [:Qbb, :Qc, :Qz, :Q12, :QJ]
+    if false
 
-    # Convert to sparse matrix
-    nsimplices = size(mesh.simplices, 1)
-    @assert size(mesh.simplices, 2) == C + 1
-    I = Int[]
-    J = Int[]
-    V = One[]
-    for j in 1:nsimplices
-        for i in @view mesh.simplices[j, :]
-            push!(I, i)
-            push!(J, j)
-            push!(V, One())
+        # Triangulate
+        mesh = delaunay(S[coords[i][c] for i in 1:nvertices, c in 1:C])
+        # [:Qbb, :Qc, :Qz, :Q12, :QJ]
+
+        # Convert to sparse matrix
+        nsimplices = size(mesh.simplices, 1)
+        @assert size(mesh.simplices, 2) == C + 1
+        I = Int[]
+        J = Int[]
+        V = One[]
+        for j in 1:nsimplices
+            for i in @view mesh.simplices[j, :]
+                push!(I, i)
+                push!(J, j)
+                push!(V, One())
+            end
         end
+        # @assert all(i -> 1 ≤ i ≤ nvertices, I)
+        # @assert all(j -> 1 ≤ j ≤ nsimplices, J)
+        simplices = sparse(I, J, V, nvertices, nsimplices)
+
+    else
+
+        # Triangulate
+        mesh = delaunay(S[coords[i][c] for c in 1:C, i in 1:nvertices],
+                        "qhull d Qt Qbb Qc Qz")
+
+        # Convert to sparse matrix
+        nsimplices = size(mesh, 2)
+        @assert size(mesh, 1) == C + 1
+        I = Int[]
+        J = Int[]
+        V = One[]
+        for j in 1:nsimplices
+            for i in @view mesh[:, j]
+                push!(I, i)
+                push!(J, j)
+                push!(V, One())
+            end
+        end
+        # @assert all(i -> 1 ≤ i ≤ nvertices, I)
+        # @assert all(j -> 1 ≤ j ≤ nsimplices, J)
+        simplices = sparse(I, J, V, nvertices, nsimplices)
     end
-    # @assert all(i -> 1 ≤ i ≤ nvertices, I)
-    # @assert all(j -> 1 ≤ j ≤ nsimplices, J)
-    simplices = sparse(I, J, V, nvertices, nsimplices)
 
     return simplices
 end
