d284396cd912ff4d9c88906bf6e97af6239c01e3
Erik Schnetter <schnetter@gmail.com>
2020-11-02 12:56:49 -0500

    Add Documeter package

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
-# Run `julia --project=@. --color=yes make.jl` to build the
-# documentation. The `Documenter` must be available (i.e. installed
-# globally) for this to work.
+# Run `julia --project=@. --color=yes make.jl` in this directory to
+# build the documentation. The `Documenter` must be available (i.e.
+# installed globally) for this to work.
 
 using Documenter
 
